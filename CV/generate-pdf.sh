fc40c31ac10e291a644fe992bb62ac513b665f4a
Andrei Punko <andd3dfx@gmail.com>
2023-12-11 13:19:15 +0300

    Remove existing PDF before generation of new

--- CV/generate-pdf.sh
+++ CV/generate-pdf.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+rm pdf/*.pdf
+
 curl -v --data-urlencode "markdown=$(cat Andrei_Punko_CV_\(eng\).md)" -d "css=$(cat style.css)" --output pdf/Andrei_Punko_CV_\(eng\).pdf https://md-to-pdf.fly.dev
