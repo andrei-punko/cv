0fe3ac51df57f26387bd20371407dab73027cab3
Andrei Punko <andd3dfx@gmail.com>
2025-07-25 20:34:37 +0300

    Try to add generation of rus CV in similar way as eng

--- CV/generate-pdf.sh
+++ CV/generate-pdf.sh
@@ -3,3 +3,5 @@
 rm pdf/*.pdf
 
 curl -v --data-urlencode "markdown=$(cat Andrei_Punko_CV_\(eng\).md)" -d "css=$(cat style.css)" --output pdf/Andrei_Punko_CV_\(eng\).pdf https://md-to-pdf.fly.dev
+
+curl -v --data-urlencode "markdown=$(cat Andrei_Punko_CV_\(rus\).md)" -d "css=$(cat style.css)" --output pdf/Andrei_Punko_CV_\(rus\).pdf https://md-to-pdf.fly.dev
