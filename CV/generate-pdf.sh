72292d0bb862e411f8f4e72555893b6fbb6f2b10
Andrei Punko <andd3dfx@gmail.com>
2025-07-25 21:15:23 +0300

    Fix .sh script. We will generate RUS version manually later

--- CV/generate-pdf.sh
+++ CV/generate-pdf.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-rm pdf/*.pdf
+rm pdf/*eng*.pdf
 
 curl -v --data-urlencode "markdown=$(cat Andrei_Punko_CV_\(eng\).md)" -d "css=$(cat style.css)" --output pdf/Andrei_Punko_CV_\(eng\).pdf https://md-to-pdf.fly.dev
-
-curl -v --data-urlencode "markdown=$(cat Andrei_Punko_CV_\(rus\).md)" -d "css=$(cat style.css)" --output pdf/Andrei_Punko_CV_\(rus\).pdf https://md-to-pdf.fly.dev
